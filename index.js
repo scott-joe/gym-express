5903d50d2bdb24aa50af29288a25f48a8bf300af
Scott Williams <scott.joe.williams@gmail.com>
2020-11-24 16:02:09 -0500

    Adds user update

--- index.js
+++ index.js
@@ -1,53 +1,149 @@
 const express = require('express')
 const fs = require('fs')
+const path = require('path')
 const _ = require('lodash')
 const cons = require('consolidate')
+const bodyParser = require('body-parser')
+const debug = require('debug')
+
+const { NODE_ENV } = process.env
+const fsOpts = { encoding: 'utf-8' }
+const log = {
+  error: debug('error'),
+  console: debug('console'),
+}
 
 // =================================
-// ========== SET UP DATA ==========
+// ========== DATA HELPERS =========
 // =================================
-// Instantiate Users as empty array
-const users = []
-// Read static users.json file and parse results
-fs.readFile('./data/users.json', { encoding: 'utf-8' }, (err, data) => {
-  if (err) throw err
+function getUserFilePath(username) {
+  return path.join(__dirname, 'users', `${username}.json`)
+}
 
-  JSON.parse(data).forEach((user) => {
-    // Add Full Name Prop
-    users.push({
-      ...user,
-      name: {
-        ...user.name,
-        full: _.startCase(`${user.name.first} ${user.name.last}`),
-      },
-    })
-  })
-})
+// Read user json file and parse results
+function getUser(username) {
+  const address = {}
+  // read specific user's file
+  const user = JSON.parse(fs.readFileSync(getUserFilePath(username), fsOpts))
+  // iterate over user's location data and apply StartCase
+  for (const [key, value] of Object.entries(user.location)) {
+    address[key] = _.startCase(value)
+  }
+
+  return {
+    ...user,
+    location: address,
+    name: {
+      ...user.name,
+      full: _.startCase(`${user.name.first} ${user.name.last}`),
+    },
+  }
+}
+
+// Save user data to file
+function saveUser(username, data) {
+  try {
+    const filename = getUserFilePath(username)
+    fs.writeFileSync(filename, JSON.stringify(data, null, 2), fsOpts)
+  } catch (error) {
+    log.error(error)
+    throw error
+  }
+}
 
 // =================================
 // ===== CONFIGURE APPLICATION =====
 // =================================
 // Instantiate Express app instance
 const app = express()
+// Set up body parser
+app.use(bodyParser.urlencoded({ extended: true }))
 // Set up images directory
 app.use('/images', express.static('images'))
+// Kill requests for the favicon
+app.get('/favicon.ico', (_req, res) => res.end())
 
-// User route
+// User routes
 app.get('/:username', (req, res) => {
   const username = req.params?.username
-  // const user = users.filter((x) => x.username === username)[0]
-  // cons.handlebars('views/user.hbs', { user: user }, function (err, html) {
-  cons.handlebars('views/user.hbs', { username }, function (err, html) {
-    if (err) throw err
-    res.send(html)
-  })
+  // Fetch the current user's data out of the user store
+  const user = getUser(username)
+
+  cons.handlebars(
+    'views/user.hbs',
+    { user: user, address: user.location },
+    function (err, html) {
+      if (err) throw err
+      res.send(html)
+    }
+  )
+})
+app.put('/:username', (req, res) => {
+  try {
+    // Get username from params
+    const username = req.params?.username
+    // Get user data from file
+    const user = getUser(username)
+    // Get location information from form submission
+    user.location = req.body
+    // save user data
+    saveUser(username, user)
+    // send response
+    // res.end()
+    res.status(200).send('User Updated')
+  } catch (error) {
+    log.error(error)
+    res.status(500).send(NODE_ENV ? 'Error updating user' : error)
+    throw error
+  }
+})
+app.delete('/:username', (req, res) => {
+  try {
+    // get username
+    const username = req.params.username
+    const filename = getUserFilePath(username)
+    // delete user file
+    fs.unlinkSync(filename)
+    res.status(200).send('User Deleted')
+  } catch (error) {
+    log.error(error)
+    res.status(500).send(NODE_ENV ? 'Error deleting user' : error)
+    throw error
+  }
 })
 
-// Set Root route handler
+// Index
 app.get('/', (_req, res) => {
-  cons.handlebars('views/index.hbs', { users }, function (err, html) {
-    if (err) throw err
-    res.send(html)
+  // Pull file list in users directory
+  fs.readdir('users', (err, files) => {
+    const users = []
+
+    // Read each file and pull data out into it's own
+    files.forEach((filename) => {
+      // Pull user's first and last name into users list
+      fs.readFile(
+        path.join(__dirname, 'users', filename),
+        fsOpts,
+        (err, data) => {
+          const user = JSON.parse(data)
+          // Format the data for rendering
+          users.push({
+            username: user.username,
+            name: {
+              full: _.startCase(`${user.name.first} ${user.name.last}`),
+            },
+          })
+
+          if (users.length === files.length) {
+            // Render the user index
+            cons.handlebars('views/index.hbs', { users }, function (err, html) {
+              if (err) throw err
+              res.send(html)
+            })
+          }
+        }
+      )
+    })
   })
 })
 
